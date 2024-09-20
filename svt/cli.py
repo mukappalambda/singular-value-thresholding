7ac5b0bb7d1b32bf8d467999f22de14ed0a7a52f
mukappalambda <maokailan24@gmail.com>
2024-09-20 10:00:09 +0800

    ✨ Feature(svt-cli): add common flags (#16)

--- svt/cli.py
+++ svt/cli.py
@@ -1,22 +1,46 @@
 from pathlib import Path
 
 import numpy as np
+import typer
 from PIL import Image
 
 from svt.common import draw_image, gen_mask, resize_image
 from svt.svt_helpers import svt_solver
 
+app = typer.Typer(name="svt-cli")
 
-def main():
+
+@app.command()
+def main(
+    fname: str = typer.Argument(
+        default=..., metavar="/path/to/image", help="input image path"
+    ),
+    random_state: int = typer.Option(default=None, min=0, help="random state"),
+    width: int = typer.Option(
+        default=500,
+        min=0,
+        help="resize width; smaller width for faster convergence.",
+    ),
+    pct: float = typer.Option(
+        default=0.1,
+        min=0.0,
+        max=1.0,
+        help="mask percentage; higher percentage for faster convergence",
+    ),
+    it: int = typer.Option(default=100, min=0, help="maximum iterations"),
+):
     """
-    Main
+    Examples:
+
+    $ svt-cli ./input.png
+
+    $ svt-cli ./input.png --pct 0.2
+
+    $ svt-cli ./input.png --it 150
     """
-    fname = "landscape.jpg"
     raw_img = Image.open(fname)
-    width = 600
     img = resize_image(raw_img, (width, width))
-    print(f"Raw image size: {raw_img.size}")  # (6000, 4000)
-    print(f"Resized image size: {img.size}")  # (2048, 1365)
+    print(f"Resized image from {img.size} to {raw_img.size}")
 
     # image to numpy array
     img = np.array(img)
@@ -26,30 +50,25 @@ def main():
 
     # for faster convergence
     scaled_img = img / 255
-    # percentage of sampled pixels
-    pct = 0.1
     # for reproducibility
-    random_state = 1234
     mask = gen_mask(A=img, pct=pct, random_state=random_state)
 
-    n_iters = 300
     X = svt_solver(
         M=scaled_img,
         mask=mask,
-        n_iters=n_iters,
+        n_iters=it,
         tol=0.1,
         random_state=random_state,
     )
     X = np.clip(X, a_min=0, a_max=1) * 255
 
-    path = Path("./output_folder")
-    impaired_img_path = path.joinpath("impaired.png")
-    recovered_img_path = path.joinpath("recovered.png")
-    path.mkdir(parents=True, exist_ok=True)
+    impaired_img_path = Path("./impaired.png")
+    recovered_img_path = Path("./recovered.png")
 
     draw_image(mask * img, height, width, impaired_img_path.as_posix())
     draw_image(X, height, width, recovered_img_path.as_posix())
+    print(f"generated images: {impaired_img_path} and {recovered_img_path}")
 
 
 if __name__ == "__main__":
-    main()
+    app()
